61fc6477fa800b9df6b78c5dc8ec2e1fdf643886
Jorge Mancilla <jorgemancilla10109@hotmail.com>
2023-02-25 17:29:00 -0600

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,16 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://itsjorge.tech",
-  integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    svelte(),
+    partytown({ config: { forward: ["dataLayer.push"] } }),
+  ],
 });
