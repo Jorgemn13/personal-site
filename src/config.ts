9b5635db5c9e9eb8a903bfb648e713e17e71902d
Jorge Mancilla <jorgemancilla10109@hotmail.com>
2023-02-25 16:26:40 -0600

    fix meta desc

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Jorge Mancilla Personal Website";
-export const SITE_DESCRIPTION = "Jorge Mancilla Personal Website";
+export const SITE_DESCRIPTION =
+  "Being a 10x Engineer is not as hard as it sounds. Just start many side projects without finishing them, learn a little bit of Rust to feel cooler, and try to learn the latest JS framework so you can seem up to date.";
